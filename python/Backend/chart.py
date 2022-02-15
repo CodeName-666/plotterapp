8f79d3c980a6c7ef4df8afc9af6666c1b62dca80
SeidC <SeidelChristof@googlemail.com>
2022-02-15 23:48:26 +0100

    .

--- python/Backend/chart.py
+++ python/Backend/chart.py
@@ -1,14 +1,42 @@
 
+import re
 import typing
-from PySide2.QtCore import QObject, Slot, Signal
+from PySide2.QtCore import QObject, Slot, Signal, Property
+from PySide2.QtCharts import QtCharts
 
 
 class Chart():
 
     new_graph = Signal(str, int)
 
-    def __init__(self, obj_name: str = "") -> None:
-        obj_name = obj_name
+    def __init__(self) -> None:
+        self._graph_list = []
+        
+        
+
+    @Slot(str,QObject, result= bool)
+    def add_graph(self ,name: str, graph: QtCharts.QLineSeries):
+        if not name in self._graph_list:
+            self._graph_list["name"] = graph
+            return True
+        else:
+            return False
+
+    @Property(QObject)
+    def chart(self):
+        try:
+            return self._chart
+        except:
+            return None
+
+    @chart.setter
+    def chart(self, chart: QtCharts.QChart):
+        self._chart = chart
+
+    @Slot(str,QObject, result= bool)
+    def append_graph_point(self, graph_name: str, point: typing.Tuple ):
+        pass
+
 
     def setup(self, dict: typing.Dict):
         pass
