c2318c7140d6309526e8c69d79b12e50ed9618fd
SeidC <SeidelChristof@googlemail.com>
2022-02-17 23:22:57 +0100

    .

--- python/Backend/chart.py
+++ python/Backend/chart.py
@@ -1,7 +1,6 @@
 
-import re
 import typing
-from PySide2.QtCore import QObject, Slot, Signal, Property
+from PySide2.QtCore import QObject, Slot, Signal, Property, QTimer
 from PySide2.QtCharts import QtCharts
 
 
@@ -10,7 +9,14 @@ class Chart():
     new_graph = Signal(str, int)
 
     def __init__(self) -> None:
-        self._graph_list = []
+        self._graph_list = {}
+        self._timer = QTimer()
+        self._timer.timeout.connect(self.loop)
+        self._timer.start(1000)
+        self._m_x =  9
+        self._m_y = 0
+        self._xPoint =  0.0
+        self._yPoint =  0.0
         
         
 
@@ -21,8 +27,12 @@ class Chart():
             return True
         else:
             return False
+    @Slot(QObject)
+    def set_chart(self, chart: QtCharts.QChart):
+        self._chart = chart
 
-    @Property(QObject)
+
+    @Property(QtCharts.QChart)
     def chart(self):
         try:
             return self._chart
@@ -31,12 +41,17 @@ class Chart():
 
     @chart.setter
     def chart(self, chart: QtCharts.QChart):
+        print("Set Chart: {}".format(self._chart))
         self._chart = chart
 
     @Slot(str,QObject, result= bool)
     def append_graph_point(self, graph_name: str, point: typing.Tuple ):
         pass
 
+    def loop(self):
+        c = self._chart
+        c.axes()
+        print("loop")
 
     def setup(self, dict: typing.Dict):
         pass
